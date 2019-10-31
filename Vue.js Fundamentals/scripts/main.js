02913515f08c337ac4c48dfddf91106332140426
asifalmani <asif.shahzad@coeus-solutions.de>
2019-10-31 14:09:27 +0500

    Vue fundamentals course folder added with source

---
+++ Vue.js Fundamentals/scripts/main.js
@@ -0,0 +1,14 @@
+// new Vue ({
+//     el: '#shopping-list',
+//     data: {
+//         header: 'Here is Vue First Exercise'
+//     }
+// });
+
+// 2nd Way
+var shoppingList = new Vue ({
+    el: '#shopping-list',
+    data: {
+        header: 'Here is Vue First Exercise'
+    }
+});
