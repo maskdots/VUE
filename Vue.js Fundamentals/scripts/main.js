7de80e7404baac8300c0deb350996eb01170c07e
asifalmani <asif.shahzad@coeus-solutions.de>
2019-10-31 17:51:28 +0500

    css file added for dynamic calsses

--- Vue.js Fundamentals/scripts/main.js
+++ Vue.js Fundamentals/scripts/main.js
@@ -13,15 +13,31 @@ var shoppingList = new Vue ({
         header: 'Shopping List App',
         newItem: '',
         items: [
-            // 'Cooking Oil 5Ltr',
-            // 'Shamppo Family Pack',
-            // 'Lux Soap 150g 12',
+            {
+                label: 'Cooking Oil 5Ltr',
+                purchased: true,
+            },
+            {
+                label: 'Shamppo Family Pack',
+                purchased: false,
+            },
+            {
+                label: 'Lux Soap 150g 12',
+                purchased: false,
+            }
         ]
     },
     methods: {
+        // saveItem: function() {
+        //     this.items.push(this.newItem);
+        //     this.newItem='';
+        // },
         saveItem: function() {
-            this.items.push(this.newItem);
-            this.newItem='';
+            this.items.push({
+                label: 'this.newItem',
+                purchased: false,
+            },);
+            this.newItem = '';
         },
         changeState: function(newState) {
             this.state = newState;
